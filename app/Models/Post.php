5971ea2549db6d054985334055c0dbe3ad8b9bfa
Eternal-Love-and-Fire <yevgenkotyk@gmail.com>
2024-03-02 22:54:28 +0200

    ep11

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public static function allPostsFiles()
+    {
+        $files = File::files(resource_path('posts/'));
+
+        return array_map(function ($file) {
+            return $file->getContents();
+        }, $files);
+    }
+
+    public static function find($slug)
+    {
+        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
+            throw new ModelNotFoundException();
+        }
+
+        return cache()->remember("posts.{$slug}", 3600, fn() => file_get_contents($path));
+    }
+}
