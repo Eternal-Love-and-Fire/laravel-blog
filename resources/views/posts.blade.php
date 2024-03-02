debbfa0393ee1eee6e3d5ed56226c562463332f6
Eternal-Love-and-Fire <yevgenkotyk@gmail.com>
2024-03-02 19:56:51 +0200

    ep7

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae excepturi saepe nulla, natus ratione, aperiam
+            harum consequuntur deserunt, numquam perferendis optio aspernatur aut doloribus nam adipisci cumque est sit!
+            Veniam?
+            Natus reprehenderit molestias veritatis autem atque enim dolorem fugit, perferendis, reiciendis tempora
+            harum temporibus illum suscipit accusantium! Delectus aspernatur, maxime repellat cum explicabo debitis ea
+            quos, animi officiis adipisci voluptatibus!
+            Nam incidunt provident aspernatur. Natus sed aliquam voluptate quia ea similique non hic. Ullam expedita
+            architecto illum fugit dolorem? Quidem minus quibusdam, eligendi voluptates nulla quos illo repellat
+            praesentium amet.</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae excepturi saepe nulla, natus ratione, aperiam
+            harum consequuntur deserunt, numquam perferendis optio aspernatur aut doloribus nam adipisci cumque est sit!
+            Veniam?
+            Natus reprehenderit molestias veritatis autem atque enim dolorem fugit, perferendis, reiciendis tempora
+            harum temporibus illum suscipit accusantium! Delectus aspernatur, maxime repellat cum explicabo debitis ea
+            quos, animi officiis adipisci voluptatibus!
+            Nam incidunt provident aspernatur. Natus sed aliquam voluptate quia ea similique non hic. Ullam expedita
+            architecto illum fugit dolorem? Quidem minus quibusdam, eligendi voluptates nulla quos illo repellat
+            praesentium amet.</p>
+    </article>
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae excepturi saepe nulla, natus ratione, aperiam
+            harum consequuntur deserunt, numquam perferendis optio aspernatur aut doloribus nam adipisci cumque est sit!
+            Veniam?
+            Natus reprehenderit molestias veritatis autem atque enim dolorem fugit, perferendis, reiciendis tempora
+            harum temporibus illum suscipit accusantium! Delectus aspernatur, maxime repellat cum explicabo debitis ea
+            quos, animi officiis adipisci voluptatibus!
+            Nam incidunt provident aspernatur. Natus sed aliquam voluptate quia ea similique non hic. Ullam expedita
+            architecto illum fugit dolorem? Quidem minus quibusdam, eligendi voluptates nulla quos illo repellat
+            praesentium amet.</p>
+    </article>
+</body>
+</html>
